c9e9b901a8eb96c083c881bd864b5a8b51324a44
EvansMutwiri <mutwirievansm@gmail.com>
2021-11-04 17:21:33 +0300

    find note by id

--- src/app/pages/note-details/note-details.component.ts
+++ src/app/pages/note-details/note-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
 
@@ -11,11 +12,27 @@ import { NotesService } from 'src/app/shared/notes.service';
 export class NoteDetailsComponent implements OnInit {
 
   note: Note = new Note;
+  //store note id
+  noteId!: number;
 
-  constructor(private notesService: NotesService) { }
+  new?: boolean;
+
+  constructor(private notesService: NotesService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     // this.note = new Note()
+
+    //know which route has been chosen between new and edit
+
+    this.route.params.subscribe((params: Params)=>{
+      if(params.id) {
+        this.note = this.notesService.get(params.id);
+        this.noteId = params.id;
+        this.new = false;
+      } else{
+        this.new = true;
+      }
+    })
   }
 
   onSubmit(form: NgForm) {
@@ -23,7 +40,17 @@ export class NoteDetailsComponent implements OnInit {
 
     //create service to store notes array and manage it
 
-    this.notesService.add(form.value)
+    if(this.new){
+      this.notesService.add(form.value);
+      this.router.navigateByUrl('/');
+    } else {
+      this.notesService.update(this.noteId, form.value.title, form.value.body);
+      this.router.navigateByUrl('/');
+    }
+  }
+
+  cancel(){
+    this.router.navigateByUrl("/")
   }
 
 }
